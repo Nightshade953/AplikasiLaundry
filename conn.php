1f9bd590dd09060deabd799d4e83463a2e208973
KepitingKikir <muhamadkyoshiro3.0@gmail.com>
2025-01-17 08:18:41 +0700

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+$server_name  = "localhost";
+$username = "root";
+$password = "";
+$db = "aplikasi_laundry";
+
+$conn = new mysqli($server_name, $username, $password, $db);
+
+if ($conn->connect_error){
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+
+?>
